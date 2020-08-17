415984136332290c65c2c59252bf213470131f27
jbnilles <jbnilles24@gmail.com>
2020-08-17 13:47:50 -0700

    add js for spliting, reversing and joining new array
    Co-authored-by: Ophelia Swen <ophelia.swen@gmail.com>
    Co-authored-by: Drew Lockhart <drewmlockhart@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,25 @@
 $(document).ready(function () {
+    $("#formOne").submit(function() {
+      event.preventDefault();
+        const sentence = $("#sentence").val();
+        //alert(sentence);
+        const sentence1 = sentence.split(' ');
+        //alert(sentence1);
+        let words = [];
+      sentence1.forEach(function(element) {
+        if (element.length >=3) {
+          words.push(element);
+        }
+
+      });
+
+      words.reverse();
+      let newSentence = words.join(' ');
+      //alert(newSentence);
+      $('#newSentence').append(newSentence);
+    });
+   
+    
 
 
 });
